780839739fa23a1df8f4a1d24774149681681a84
saikiran <skiran252@gmail.com>
2021-09-27 15:12:42 +0530

    emoji identifier v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import EmojiDictionary from "./emoji.json";
 function App() {
+  const [message, setMessage] = useState("identified emoji will be shown here");
+
+  const identifyEmoji = (e) => {
+    let emojiName = "Could not recognize this emoji";
+    const emoji = e.target.value;
+    EmojiDictionary.forEach((element) => {
+      if (element.emoji === emoji) {
+        emojiName = element.description;
+      }
+    });
+    setMessage(emojiName);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="text-center mt-5 mb-3">Emoji Identifier</h1>
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Enter your Emoji here"
+        onChange={identifyEmoji}
+      />
+      <div className="mt-3 fw-bolder fs-3">{message}</div>
+      <div>
+        {EmojiDictionary.map((emoji, index) => {
+          return (
+            <span key={index} className="fs-3 fw-bold p-2">
+              {emoji.emoji}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
